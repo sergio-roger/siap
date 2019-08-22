718332ece35a9afda2e9364a54c1039db53c583d
sergio roger <sergio.lkg.31@gmail.com>
2019-08-22 14:19:45 -0500

    Ultimos cambios

---
+++ SistemaPedidosAutomatizado/Modulo/Efectos.vb
@@ -0,0 +1,42 @@
+﻿Module Efectos
+
+    Public Sub entradaDevanecer(ByRef formulario As Form)
+        Dim efecto As Double
+
+        For efecto = 0.0 To 1.0 Step 0.1
+            formulario.Opacity = efecto
+            formulario.Refresh()
+            Threading.Thread.Sleep(100)
+        Next
+    End Sub
+
+    Public Sub salidaDevanecer(ByRef formulario As Form)
+        Dim efecto As Double
+
+        For efecto = 1.1 To 0.0 Step 0.1
+            formulario.Opacity = efecto
+            formulario.Refresh()
+            Threading.Thread.Sleep(100)
+        Next
+    End Sub
+
+    Public Sub entradaDevanecer()
+        Dim efecto As Double
+
+        For efecto = 0.0 To 1.1 Step 0.1
+            frm_login.Opacity = efecto
+            frm_login.Refresh()
+            Threading.Thread.Sleep(100)
+        Next
+    End Sub
+
+    Public Sub salidaDevanecer()
+        Dim efecto As Double
+
+        For efecto = 1.1 To 0.0 Step 0.1
+            frm_login.Opacity = efecto
+            frm_login.Refresh()
+            Threading.Thread.Sleep(100)
+        Next
+    End Sub
+End Module
